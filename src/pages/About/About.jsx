c97bcbe893c6d552354783f6857f1e398307ba39
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-05-28 14:55:15 -0300

    Mudanças de imagem e dinamica do projeto

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,32 @@
 import styles from "../About/About.module.css";
+import Typewriter from "typewriter-effect";
 
 function About() {
   return (
     <>
       <div className={styles.container}>
+        <div className={styles.containerRight}>
+          <img
+            src="/assets/intro-dev.png"
+            alt="Tela com codigos em JavaScript"
+          />
+        </div>
         <div className={styles.containerLeft}>
           <h1>Sobre mim:</h1>
           <p style={{ color: "#95a5a6" }}>
-            Olá, eu sou um Desenvolvedor Front-End <br /> e me chamo Jonathan!
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    "Olá, eu sou um Front-End Developer e me chamo Jonathan!",
+                  )
+                  .start();
+              }}
+              options={{
+                delay: 50,
+                cursor: "|",
+              }}
+            />
           </p>
           <p>
             Estudante de Análise e Desenvolvimento de Sistemas em transição de
@@ -16,13 +35,6 @@ function About() {
             sólidos conhecimentos em lógica de programação, JavaScript e
             frameworks modernos, aliados à minha experiência prévia em produção.
           </p>
-        </div>
-
-        <div className={styles.containerRight}>
-          <img
-            src="/assets/pagina-programacao.jpg"
-            alt="Tela com codigos em JavaScript"
-          />
         </div>
       </div>
     </>
